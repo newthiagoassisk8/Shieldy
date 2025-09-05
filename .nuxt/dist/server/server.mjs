bf6706da770be0e9de3bf3e6357eba992ba7bc6c
Tiago França <github@tiagofranca.com>
2025-09-05 11:30:05 -0300

    initial commit

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from 'file:///tmp/nuxt-example/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs';
---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const version = 4;
+</script>
+
+<template>
+    <h1>Hello Nuxt {{ version }}!</h1>
+</template>
+
+<style scoped>
+h1 {
+    font-size: 3rem;
+}
+</style>
